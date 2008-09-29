d9c2ab4efd55723bf4169470dbd94766d2790ec1
Cory G Watson <gphat@cpan.org>
2008-09-29 16:41:52 +0000

    Import of GPHAT/Document-Writer-0.09 from CPAN.
    gitpan-cpan-distribution: Document-Writer
    gitpan-cpan-version:      0.09
    gitpan-cpan-path:         GPHAT/Document-Writer-0.09.tar.gz
    gitpan-cpan-author:       GPHAT
    gitpan-cpan-maturity:     released

--- lib/Document/Writer.pm
+++ lib/Document/Writer.pm
@@ -7,160 +7,108 @@ use Forest;
 use Paper::Specs units => 'pt';
 
 use Document::Writer::Page;
-use Document::Writer::TextLayout;
 
 our $AUTHORITY = 'cpan:GPHAT';
-our $VERSION = '0.08';
+our $VERSION = '0.09';
 
-has 'default_color' => (
-    is => 'rw',
-    isa => 'Graphics::Color',
-    required => 1
-);
-has 'current_page' => (
-    is => 'rw',
-    isa => 'Num',
-);
-has 'pages' => (
+has 'components' => (
     metaclass => 'Collection::Array',
     is => 'rw',
-    isa => 'ArrayRef[Document::Writer::Page]',
+    isa => 'ArrayRef[Graphics::Primitive::Component]',
     default => sub { [] },
     provides => {
-        'clear'=> 'clear_pages',
-        'count'=> 'page_count',
-        'get' => 'get_page',
-        'push' => 'add_page',
-        'first'=> 'first_page',
-        'last' => 'last_page'
+        'clear'=> 'clear_components',
+        'count'=> 'component_count',
+        'get' => 'get_component',
+        'push' => 'add_component',
+        'first'=> 'first_component',
+        'last' => 'last_component'
     }
 );
-
-sub add_text_to_page {
-    my ($self, $driver, $font, $text, $color) = @_;
-
-    my $curr_page = $self->get_page($self->current_page);
-    # TODO Orientation...
-
-    unless(defined($driver->width)) {
-        $driver->width($curr_page->width);
-        $driver->height($curr_page->height);
-    }
-
-    my $width = $curr_page->inside_width;
-
-    my $tl = Document::Writer::TextLayout->new(
-        font => $font,
-        text => $text,
-        width => $width,
-    );
-
-    $tl->layout($driver);
-
-    my $used = 0;
-    my $tlh = $tl->height;
-
-    # XXX ?
-    $driver->prepare($curr_page);
-    $curr_page->layout_manager->do_layout($curr_page);
-
-    while($used < $tlh) {
-        my $avail = $curr_page->body->inside_height - $curr_page->body->layout_manager->used->[1];
-        if($avail <= 0) {
-            $curr_page = $self->next_page;
-            next;
-        }
-        my $lsize = $avail;
-        if($tlh < $used + $avail) {
-            $lsize = $tlh - $used;
-        }
-        my $lines = $tl->slice($used, $lsize);
-
-        if($lines->{size} <= 0) {
-            # XXX
-            # If we get back nothing then we must've asked for a size too
-            # small to get back data.  Make a new page.  This will eventually
-            # cause an endless loop...
-            $curr_page = $self->next_page;
-            next;
-        }
-
-        my $tb = Graphics::Primitive::TextBox->new(
-            color => defined($color) ? $color : $curr_page->color,
-            font => $font,
-            lines => $lines->{lines},
-            minimum_width => $curr_page->width,
-            minimum_height => $lines->{size}
-        );
-        # XXX FIX ME
-        # $tb->background_color(Graphics::Color::RGB->new(red => rand(1), green => rand(1), blue => rand(1), alpha => .25));
-        $curr_page->body->add_component($tb);
-        $driver->prepare($curr_page);
-        # $curr_page->prepare;
-        $curr_page->layout_manager->do_layout($curr_page);
-        $used += $lines->{size};
-    }
-}
-
-sub add_to_page {
-    my ($self, $driver, $thing) = @_;
-
-    return unless defined($thing);
-
-    # If current_page isn't set, assume they want to operate on the last page.
-    unless(defined($self->current_page)) {
-        $self->current_page(scalar(@{ $self->pages }) - 1);
-    }
-    unless(defined($self->current_page)) {
-        # Well, shit. We still don't have a page.  Bitch about it since we
-        # can't create one without a size.
-        croak('No pages to add to.');
-    }
-
-    my $page = $self->get_page($self->current_page);
-    croak 'current_page refers to an undefined page' unless defined($page);
-
-    # TODO orientation...
-    if($thing->height > $page->height) {
-        croak 'requested component is larger than page height';
-    }
-
-    if(ref($thing) && $thing->isa('Graphics::Primitive::Component')) {
-        $thing->prepare($driver);
-    } else {
-        croak('add_to_page requires a Graphics::Primitive::Component');
-    }
-
-    my $avail = $page->body->inside_height - $page->body->layout_manager->used->[1];
-    # Orientation?
-    if($avail < $thing->height) {
-        $page = $self->next_page;
-        # $page->prepare;
-        # $page->layout_manager->do_layout($page);
-
-        $self->add_to_page($driver, $thing);
-
-        return;
-    }
-
-    $page->body->add_component($thing);
-    $page->prepare;
-    $page->layout_manager->do_layout($page);
-
-    # TODO FIx avail and new page!
-
-    # if($page->layout_manager->overflow) {
-    #     # We overflowed.  Time to move to the next page and try there.
-    #     $page->pop_component;
-    #     $self->next_page;
-    #     $self->add_to_page($thing);
-    # }
-}
+has 'last_page' => (
+    is => 'rw',
+    isa => 'Document::Writer::Page',
+);
 
 sub draw {
     my ($self, $driver) = @_;
 
-    foreach my $p (@{ $self->pages }) {
+    my @pages;
+    foreach my $c (@{ $self->components }) {
+
+        next unless(defined($c));
+
+        $driver->prepare($c);
+
+        if($c->isa('Document::Writer::Page')) {
+            # $driver->prepare($c);
+            $c->layout_manager->do_layout($c);
+
+            push(@pages, $c);
+        } else {
+            #  Seed the current page
+            my $currpage = $pages[-1];
+
+            die('First component must be a Page') unless defined($currpage);
+
+            if($c->isa('Document::Writer::TextArea')) {
+                $c->width($currpage->inside_width);
+                my $layout = $driver->get_textbox_layout($c);
+                my $lh = $layout->height;
+
+                my $used = 0;
+
+                while($used < $lh) {
+                    my $avail = $currpage->body->inside_height
+                        - $currpage->body->layout_manager->used->[1];
+                    if($avail == 0) {
+                        $currpage = $self->add_page_break($driver);
+                        push(@pages, $currpage);
+                        $driver->prepare($currpage);
+                        $currpage->layout_manager->do_layout($currpage);
+                        $avail = $currpage->body->inside_height
+                            - $currpage->body->layout_manager->used->[1];
+                        next;
+                    }
+
+                    if($avail > $layout->height) {
+                        my $tb = $layout->slice($used);
+                        $currpage->body->add_component($tb);
+
+                        $used += $tb->minimum_height;
+                    } else {
+                        # print "B\n";
+                        my $tb = $layout->slice($used, $avail);
+                        # print "AXXX: $used $avail\n";
+                        # print "MH: ".$tb->minimum_height."\n";
+                        $currpage->body->add_component($tb);
+                        $used += $tb->minimum_height;
+                    }
+                    $driver->prepare($currpage);
+                    $currpage->layout_manager->do_layout($currpage);
+                    $currpage = $self->add_page_break($driver);
+                }
+            } else {
+                my $pageadded = 0 ;
+                my $avail = $currpage->body->inside_height
+                    - $currpage->body->layout_manager->used->[1];
+                if($avail >= $c->height) {
+                    $currpage->add_component($c);
+                    $driver->prepare($c);
+                    $c->layout_manager->do_layout($c);
+                } else {
+                    if($pageadded) {
+                        die("Stopping possible endless loop: $c too big for page");
+                    }
+                    $pageadded = 1;
+                    $self->add_page_break($driver);
+                    push(@pages, $currpage);
+                }
+            }
+        }
+    }
+
+    foreach my $p (@pages) {
 
         # Prepare all the pages...
         $driver->prepare($p);
@@ -171,18 +119,26 @@ sub draw {
         $driver->finalize($p);
         $driver->reset;
         $driver->draw($p);
+        # use Forest::Tree::Writer::ASCIIWithBranches;
+        # my $t = Forest::Tree::Writer::ASCIIWithBranches->new(tree => $p->get_tree);
+        # print $t->as_string;
     }
+
+    return \@pages;
 }
 
 sub find {
     my ($self, $predicate) = @_;
 
     my $newlist = Graphics::Primitive::ComponentList->new;
-    foreach my $page (@{ $self->pages }) {
+    foreach my $c (@{ $self->components }) {
 
-        return unless(defined($page));
+        return unless(defined($c));
 
-        my $list = $page->find($predicate);
+        unless($c->can('components')) {
+            return $newlist;
+        }
+        my $list = $c->find($predicate);
         if(scalar(@{ $list->components })) {
             $newlist->push_components(@{ $list->components });
             $newlist->push_constraints(@{ $list->constraints });
@@ -190,15 +146,6 @@ sub find {
     }
 
     return $newlist;
-}
-
-sub find_page {
-    my ($self, $name) = @_;
-
-    foreach my $p ($self->pages) {
-        return $p if($p->name eq $name);
-    }
-    return undef;
 }
 
 sub get_paper_dimensions {
@@ -217,49 +164,39 @@ sub get_tree {
 
     my $tree = Forest::Tree->new(node => $self);
 
-    foreach my $p (@{ $self->pages }) {
-        $tree->add_child($p->get_tree);
+    foreach my $c (@{ $self->components }) {
+        $tree->add_child($c->get_tree);
     }
 
     return $tree;
 }
 
-sub next_page {
-    my ($self, $width, $height) = @_;
-
-    if(defined($self->current_page)) {
-        my $epage = $self->pages->[$self->current_page + 1];
-        if(defined($epage)) {
-            $self->current_page($self->current_page + 1);
-            return $epage;
-        }
-    }
+sub add_page_break {
+    my ($self, $driver, $page) = @_;
 
     my $newpage;
-    if($width && $height) {
-        $newpage = Document::Writer::Page->new(
-            width => $width, height => $height,
-            color => $self->default_color
-        );
+    if(defined($page)) {
+        $newpage = $page;
     } else {
-        my $currpage = $self->last_page;
-        if($currpage) {
-            $newpage = Document::Writer::Page->new(
-                color => $currpage->color,
-                width => $currpage->width, height => $currpage->height
-            );
-        } else {
-            croak("Need a height and width for first page.");
-        }
+        die('Must add a first page to create implicit ones.') unless defined($self->last_page);
+        my $last = $self->last_page;
+        $newpage = Document::Writer::Page->new(
+            color   => $last->color,
+            width   => $last->width,
+            height  => $last->height,
+        );
     }
-    $self->add_page($newpage);
-    $self->current_page(scalar(@{ $self->pages }) - 1);
 
-    $newpage->prepare;
+    $driver->prepare($newpage);
     $newpage->layout_manager->do_layout($newpage);
+
+    $self->add_component($newpage);
+    $self->last_page($newpage);
 
     return $newpage;
 }
+
+__PACKAGE__->meta->make_immutable;
 
 1;
 __END__
@@ -270,17 +207,23 @@ Document::Writer - Library agnostic document creation
 =head1 SYNOPSIS
 
     use Document::Writer;
+    use Graphics::Color::RGB;
     # Use whatever you like
-    use Graphics::Primitive::Driver::Cairo;
+    use Graphics::Primitive::Driver::CairoPango;
 
-    my $doc = Document::Writer->new(
-        default_color => Graphics::Color::RGB->new(...)
-    );
+    my $doc = Document::Writer->new;
+    my $driver = Graphics::Primitive::Driver::CairoPango->new(format => 'pdf');
+    
     # Create the first page
-    my $p = $doc->next_page(Document::Writer->get_paper_dimensions('letter'));
-    $doc->add_text_to_page($driver, $long_multiline_text);
-    my $p2 = $doc->next_page;
-    # ... Do some other stuff
+    my @dim = Document::Writer->get_paper_dimensions('letter');
+    my $p = Document::Writer::Page->new(
+        color => Graphics::Color::RGB->new(red => 0, green => 0, blue => 0),
+        width => $dim[0], height => $dim[1]
+    );
+
+    $doc->add_page_break($driver, $page);
+    ...
+    $doc->add_component($textarea);
     $self->draw($driver);
     $driver->write('/Users/gphat/foo.pdf');
 
@@ -292,11 +235,9 @@ creating documents and a library-agnostic base for the embedding of other
 components that use Graphics::Primitive.
 
 When you create a new Document::Writer, it has no pages.  You can add pages
-to the document using either C<add_page($page)> or C<next_page>.  If calling
-next_page to create your first page you'll need to provide a width and height
-(which can conveniently be gotten from C<get_paper_dimensions>).  Subsequent
-calls to C<next_page> will default the newly created page to the size of the
-last page in the document.
+to the document using either C<add_page_break($driver, [ $page ])>.  The first
+time this is called, a page must be supplied.  Subsequent calls will clone the
+last page that was passed in.
 
 =head1 WARNING
 
@@ -308,41 +249,30 @@ pages easier than using L<Graphics::Primitive> directly.
 
 =over 4
 
-=item I<add_text_to_page ($driver, $font, $string, [$color])>
+=item I<add_component>
 
-Adds the text supplied to the current page.  Lines are automatically wrapped
-based on the current page's width.  If the text exceeds the space available
-on the current page then a new page will be created via C<next_page>.  This
-will be repeated until all the text has been added to the document.
+Add a component to this document.
 
-The supplied font will be used to determine how to display the text.
+=item I<add_page_break ($driver, [ $page ])>
 
-=item I<add_to_page ($driver, $component)>
+Add a page break to the document.  The first time this is called, a page must
+be supplied.  Subsequent calls will clone the last page that was passed in.
 
-Add a L<Graphics::Primitive::Component> to the page.  The component must
-have a width and height set.  If the component exceeds the space available
-on the current page then another page will be added via C<next_page>.  If the
-component is bigger than the current page size except bad things to happen.
-
-=item I<add_page ($page)>
-
-Add an already created page object to this document.
-
-=item I<clear_pages>
+=item I<clear_components>
 
 Remove all pages from this document.
 
-=item I<current_page>
+=item I<last_component>
 
-The index of the current page.  This value is updated when calling next_page
-and is undefined when the document is created. TODO
+The last component in the list.
 
 =item I<draw ($driver)>
 
 Convenience method that hides all the Graphics::Primitive magic when you
 give it a driver.  After this method completes the entire document will have
 been rendered into the driver.  You can retrieve the output by using
-L<Driver's|Graphics::Primitive::Driver> I<data> or I<write> methods.
+L<Driver's|Graphics::Primitive::Driver> I<data> or I<write> methods.  Returns
+the list of Page's as an arrayref.
 
 =item I<find ($CODEREF)>
 
@@ -363,51 +293,15 @@ left around after a remove_component) are automatically skipped.
 If no matching components are found then a new list is returned so that simple
 calls liked $container->find(...)->each(...) don't explode.
 
-=item I<find_page ($name)>
-
-Finds a page by name, if it exists.
-
-=item I<first_page>
-
-Return the first page.
-
 =item I<get_paper_dimensions>
 
 Given a paper name, such as letter or a4, returns a height and width in points
 as an array.  Uses L<Paper::Specs>.
 
-=item I<get_page ($pos)>
-
-Returns the page at the given position
-
 =item I<get_tree>
 
 Returns a L<Forest::Tree> object with this document at it's root and each
 page (and it's children) as children.  Provided for convenience.
-
-=item I<next_page ([$width, $height])>
-
-Return the next page. Increments the C<current_page> by one.  If a page exists
-at that index it is returned.  If a page does not exist then a new page is
-added to this document.
-
-If there are pages already in the document then width and height information
-will be copied from the last page.  Prodiving width and height as arguments
-to this method override that behaviour and are necessary if there are no pages
-from which to copy it.
-
-Note: Color is copied from the last page.  If there is no last page then
-the C<default_color> is used.
-
-For less sugar use I<add_page>.
-
-=item I<page_count>
-
-Get the number of pages in this document.
-
-=item I<pages>
-
-Get the pages in this document.
 
 =back
 
--- t/001-document.t
+++ t/001-document.t
@@ -1,35 +1,49 @@
 use Test::More tests => 7;
+use lib qw(t t/lib);
 
 use Graphics::Color::RGB;
 
 use Document::Writer;
 use Document::Writer::Page;
+use MockDriver;
 
-my $doc = Document::Writer->new(
-    default_color => Graphics::Color::RGB->new(red => 0, green => 0, blue => 0, alpha => 1)
-);
+my $doc = Document::Writer->new;
+
+my $driver = MockDriver->new;
+
 isa_ok($doc, 'Document::Writer');
 
 my ($w, $h) = Document::Writer->get_paper_dimensions('letter');
 
 eval {
-    $doc->next_page;
+    $doc->add_page_break;
 };
-ok($@ =~ /Need a height/, 'next_page with no pages');
+ok($@ =~ /Must add a first page/, 'add page break with no pages');
 
-my $tpage = $doc->next_page($w, $h);
-cmp_ok($doc->page_count, '==', 1, '1 page');
-
+my ($width, $height) = $doc->get_paper_dimensions('letter');
 my $page = Document::Writer::Page->new(
+    width => $width,
+    height => $height,
+    color => Graphics::Color::RGB->new(
+        red => 0, green => 0, blue => 0, alpha => 1
+    )
+);
+
+$doc->add_page_break($driver, $page);
+cmp_ok($doc->component_count, '==', 1, '1 component');
+
+my $page2 = Document::Writer::Page->new(
     width => $w,
     height => $h,
-    color => Graphics::Color::RGB->new(red => 0, green => 0, blue => 0, alpha => 1)
+    color => Graphics::Color::RGB->new(
+        red => 0, green => 0, blue => 0, alpha => 1
+    )
 );
-isa_ok($page, 'Document::Writer::Page');
+isa_ok($page2, 'Document::Writer::Page');
 
-$doc->add_page($page);
-cmp_ok($doc->page_count, '==', 2, '2 pages');
+$doc->add_page_break($driver, $page2);
+cmp_ok($doc->component_count, '==', 2, '2 pages');
 
-my $newpage = $doc->next_page;
-cmp_ok($newpage->width, '==', $page->width, 'new page width');
-cmp_ok($newpage->height, '==', $page->height, 'new page height');
+my $newpage = $doc->add_page_break($driver);
+cmp_ok($newpage->width, '==', $page2->width, 'new page width');
+cmp_ok($newpage->height, '==', $page2->height, 'new page height');
